68144480933d95082407185a67646bedaa78af26
Tomáš Šromovský <tomas.sromovsky@posam.sk>
2020-05-30 18:28:18 +0200

    line charts

--- static/index.js
+++ static/index.js
@@ -249,6 +249,96 @@ function streamStart(){
 }
 
 //
+// GRAPH
+//
+
+let graphType = 'acceleration';
+
+const labelsData = [];
+const accelerationData = [];
+const rotationRateData = [];
+const orientationData = [];
+
+
+const ctx = document.getElementById('myChart');
+
+const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'X',
+            data: [],
+            fill: false,
+            borderColor: [
+                'rgb(133,255,99)'
+            ],
+            borderWidth: 1
+        },{
+            label: 'Y',
+            data: [],
+            fill: false,
+            borderColor: [
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        }, {
+            label: 'Z',
+            data: [],
+            fill: false,
+            borderColor: [
+                'rgb(99,151,255)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'AJKLbjh'
+        },
+        scales: {
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Data IDs'
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    min: -1,
+                    max: 1
+                }
+            }]
+        }
+    }
+});
+
+function updateGraph() {
+    chart.data.labels = labelsData;
+
+    switch (graphType) {
+        case 'acceleration':
+            chart.data.datasets[0].data = accelerationData.map(i => i.x);
+            chart.data.datasets[1].data = accelerationData.map(i => i.y);
+            chart.data.datasets[2].data = accelerationData.map(i => i.z);
+            break;
+        case 'rotationRate':
+            chart.data.datasets[0].data = rotationRateData.map(i => i.x);
+            chart.data.datasets[1].data = rotationRateData.map(i => i.y);
+            chart.data.datasets[2].data = rotationRateData.map(i => i.z);
+            break;
+        case 'orientation':
+            chart.data.datasets[0].data = orientationData.map(i => i.x);
+            chart.data.datasets[1].data = orientationData.map(i => i.y);
+            chart.data.datasets[2].data = orientationData.map(i => i.z);
+            break;
+    }
+
+    chart.update();
+}
+
+//
 //STREAM DATA
 //
 var dataId = 0;
@@ -265,6 +355,21 @@ function streamData(){
         rotationRate: checkboxRotationRate.checked ? currentRotationRate : undefined,
         orientation: checkboxOrientation.checked ? currentOrientation : undefined
     });
+
+
+    const count = chart.data.labels.length;
+    if (count > 20) {
+        labelsData.shift();
+        accelerationData.shift();
+        rotationRateData.shift();
+        orientationData.shift();
+    }
+
+    labelsData.push(dataId);
+    accelerationData.push({x: currentAcceleration[0], y: currentAcceleration[1], z: currentAcceleration[2]});
+    rotationRateData.push({x: currentRotationRate[0], y: currentRotationRate[1], z: currentRotationRate[2]});
+    orientationData.push({x: currentOrientation[0], y: currentOrientation[1], z: currentOrientation[2]});
+    updateGraph();
 
     consoleLog("OUT", data);
     webSocket.send(data);
@@ -283,4 +388,62 @@ function streamStop(){
     enableAction([connectionButton]);
 
     consoleLog("OUT", "Stream stopped");
+}
+
+//
+// NAVIGATION
+//
+
+graphSection = document.getElementById('graph');
+const consoleFilterBox = document.getElementById('console-filter-box');
+const navItemConsole = document.getElementById('console-nav-item');
+const navItemAcceleration = document.getElementById('graph-nav-item-acceleration');
+const navItemRotationRate = document.getElementById('graph-nav-item-rotation-rate');
+const navItemOrientation = document.getElementById('graph-nav-item-orientation');
+
+function consoleVisibility(value) {
+
+    if (value) {
+        consoleSection.classList.remove('element-hidden');
+        consoleFilterBox.classList.remove('element-hidden');
+    } else {
+        consoleSection.classList.add('element-hidden');
+        consoleFilterBox.classList.add('element-hidden');
+    }
+}
+
+function graphVisibility(value) {
+
+    if (value) {
+        graphSection.classList.remove('element-hidden');
+    } else {
+        graphSection.classList.add('element-hidden');
+    }
+}
+
+navItemConsole.onclick = () => {
+    consoleVisibility(true);
+    graphVisibility(false);
+}
+
+navItemAcceleration.onclick = () => {
+    graphType = 'acceleration';
+    chart.options.title.text = 'Acceleration';
+    consoleVisibility(false);
+    graphVisibility(true);
+    updateGraph();
+}
+navItemRotationRate.onclick = () => {
+    graphType = 'rotationRate';
+    chart.options.title.text = 'Rotation Rate';
+    consoleVisibility(false);
+    graphVisibility(true);
+    updateGraph();
+}
+navItemOrientation.onclick = () => {
+    graphType = 'orientation';
+    chart.options.title.text = 'Orientation';
+    consoleVisibility(false);
+    graphVisibility(true);
+    updateGraph();
 }
